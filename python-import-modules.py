a80964bc097e5a1bc94b49052f119bef6ebf990f
Alim <21130939+ladha@users.noreply.github.com>
2018-01-09 23:38:45 -0700

    Initial commit.

---
+++ python-import-modules.py
@@ -0,0 +1,22 @@
+# Python Import Modules
+
+# this is a single line comment
+
+"""
+This is a multi-line comment.
+You can also use triple single quotes.
+"""
+
+# import statements should be at the top of a file (good coding practice)
+
+import numpy
+
+arr = numpy.array([1, 2, 3])
+
+print(arr)
+
+import numpy as np
+
+arr = np.array([2, 3, 4])
+
+print(arr)
---
+++ python-variable-types.py
@@ -0,0 +1,37 @@
+# Python Variable Types
+
+# Integers
+a = 1
+b = 2
+
+print(a + b)
+# 3
+
+# Floats
+a = 1.5
+b = 2.2
+
+print(a + b)
+# 3.7
+
+# Strings
+a = 'A string in single quotes.'
+b = "A string in double quotes."
+c = "This string is double quoted, but it's cool because it has a single quote."
+
+print(a + ' ' + b)
+# A string in single quotes. A string in double quotes.
+
+# find the type of a variable
+a = 1
+b = 1.5
+c = 'two'
+
+print(type(a))
+print(type(b))
+print(type(c))
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+
+
